3ce1b054d3a0ba20ace24a072fa3bc82e34dcab2
Alexander Collings [Student] <alexander.collings@student.sl.on.ca>
2019-09-30 15:53:16 -0400

    Added firing scripts to guns

---
+++ Assets/Scripts/AssaultRifle.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class AssaultRifle : Gun
+{
+    protected override void Update()
+    {
+        base.Update();
+        {
+            base.Update();
+            // Automatic Fire
+            if (Input.GetMouseButton(0) && Time.time - lastFireTime > fireRate)
+            {
+                lastFireTime = Time.time;
+                Fire();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Pistol.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Pistol : Gun
+{
+    protected override void Update()
+    {
+        base.Update();
+        // Shotgun and pistol are semi-automatic
+        if (Input.GetMouseButtonDown(0) && (Time.time - lastFireTime) > fireRate)
+        {
+            lastFireTime = Time.time;
+            Fire();
+        }
+    }
+}
