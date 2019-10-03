91ab23c1a9919d1e2576093c785d1a8fbf870b0b
Alex Collings <alexander.collings@student.sl.on.ca>
2019-10-03 00:23:17 -0400

    Added randomized spawning and respawning of pickups

---
+++ Assets/Scripts/PickupSpawn.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupSpawn : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] pickups;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Creates a pickup to start the game
+        SpawnPickup();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnPickup()
+    {
+        // Instantiates a random pickup to the location of the PickupSpawn GameObject
+        GameObject pickup = Instantiate(pickups[Random.Range(0, pickups.Length)]);
+        pickup.transform.position = transform.position;
+        pickup.transform.parent = transform;
+    }
+
+    IEnumerator RespawnPickup()
+    {
+        // Waits 20 seconds to spawn a new pickup
+        yield return new WaitForSeconds(20);
+        SpawnPickup();
+    }
+
+    public void PickupWasPickedUp()
+    {
+        StartCoroutine("RespawnPickup");
+    }
+}
