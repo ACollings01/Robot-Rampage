3dc9f3a79f521bf583da2017dbf6c165eef883b8
Alex Collings <alexander.collings@student.sl.on.ca>
2019-10-03 00:39:04 -0400

    Added robot spawning locations and set them to spawn at the start of the game

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    private static Game singleton;
+
+    [SerializeField]
+    RobotSpawn[] spawns;
+
+    public int enemiesLeft;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Initialize the singleton
+        singleton = this;
+        SpawnRobots();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void SpawnRobots()
+    {
+        // Spawn one robot on each spawn and increment the number of enemiesLeft
+        foreach (RobotSpawn spawn in spawns)
+        {
+            spawn.SpawnRobot();
+            enemiesLeft++;
+        }
+    }
+}
---
+++ Assets/Scripts/RobotSpawn.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotSpawn : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] robots;
+
+    private int timesSpawned;
+    private int healthBonus = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SpawnRobot()
+    {
+        // Increment number of times spawned
+        timesSpawned++;
+        // Increase health bonus to increase difficulty
+        healthBonus += 1 * timesSpawned;
+        // Create a new robot and assign its health and position
+        GameObject robot = Instantiate(robots[Random.Range(0, robots.Length)]);
+        robot.transform.position = transform.position;
+        robot.GetComponent<Robot>().health += healthBonus;
+    }
+}
