addb9df6aab8f4b422ec68aa02d1d2fe428411e5
Alex Collings <alexander.collings@student.sl.on.ca>
2019-10-03 02:13:19 -0400

    Added music

---
+++ Assets/Scripts/BackgroundMusic.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusic : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Ensures this game object won't be destroy upon loading a new screen
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
